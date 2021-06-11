da8435d71f1448f2f2c88fc6f70e2dc8c929a608
James Kelley <jkkelley86@gmail.com>
2021-06-11 16:21:20 -0500

    added imports, and App is wrapped in Router

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+
+// ⬇ What we need to import
+import { Route, HashRouter as Router } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
-
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Feedback!</h1>
+            <h4>Don't forget it!</h4>
+          </header>
+        </div>
+      </Route>
+    </Router>
   );
 }
 
