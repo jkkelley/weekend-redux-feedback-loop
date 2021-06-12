7b5309c6582023df363f21a23eebed6d3c28748b
James Kelley <jkkelley86@gmail.com>
2021-06-12 09:53:31 -0500

    delete route works

--- server/routes/feedBack.router.js
+++ server/routes/feedBack.router.js
@@ -27,4 +27,38 @@ router.post("/", (req, res) => {
     });
 });
 
+// GET some of that delicious data for our Admin
+router.get("/", (req, res) => {
+  // Bring me all of the Feedback results from DB
+  pool
+    .query(`SELECT * FROM "feedback";`)
+    .then((result) => {
+      // Send back all the rows
+      res.send(result.rows);
+    })
+    .catch((error) => {
+      console.log(`Hey Capt., Seems we're lost in space... ${error}`);
+      res.sendStatus(500);
+    });
+});
+
+// DELETE -- Tell goodbye on the way out!
+router.delete("/:id", (req, res) => {
+  const feedbackToDelete = [req.params.id];
+  // Have them wash their hands
+  const queryText = `
+    DELETE FROM "feedback" WHERE "id" = $1;
+  `;
+  // Let's take a dip, hope the water isn't cold...
+  pool
+    .query(queryText, feedbackToDelete)
+    .then((result) => {
+      // Send them back an OK
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(`Server is on the fritz... ${error}`);
+    });
+});
+
 module.exports = router;
--- src/components/AdminONLY/AdminONLY.jsx
+++ src/components/AdminONLY/AdminONLY.jsx
@@ -1,15 +1,79 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
+// Material-ui imports
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 function AdminOnly() {
-    // need an axios GET promise
-    // need a axios DELETE promise
-    // display GET to table
+  const [results, setResults] = useState([]);
+  // need an axios GET promise
+  const pastFeedbackResults = () => {
+    axios
+      .get("/feedback")
+      .then((response) => {
+        console.log(response.data);
+        setResults(response.data);
+      })
+      .catch((error) => {
+        console.log(`Seems like the server had an ${error}`);
+      });
+  };
+  // need a axios DELETE promise
+  const handleDelete = (feedback) => {
+    console.log("you clicked handleDelete");
+    console.log(feedback);
+    axios
+      .delete(`/feedback/${feedback.id}`)
+      .then((response) => {
+        console.log(`The Server says... ${response.data}`);
+        // Redraw the table
+        pastFeedbackResults();
+      })
+      .catch((error) => {
+        console.log(`Sorry, couldn't handle the delete request: ${error}`);
+      });
+  };
+  // display GET to table
+  useEffect(() => {
+    pastFeedbackResults();
+  }, []);
   return (
     <>
       <h2>Feedback Results!</h2>
       <div className="feedback-results-table-container">
-        <p>TABLE HERE!</p>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Feeling</TableCell>
+                <TableCell>Comprehension</TableCell>
+                <TableCell>Support</TableCell>
+                <TableCell>Comments</TableCell>
+                <TableCell>Delete</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {results.map((feedback, i) => (
+                <TableRow key={i}>
+                  <TableCell>{feedback.feeling}</TableCell>
+                  <TableCell>{feedback.understanding}</TableCell>
+                  <TableCell>{feedback.support}</TableCell>
+                  <TableCell>{feedback.comments}</TableCell>
+                  <TableCell>
+                    <DeleteForeverIcon onClick={() => handleDelete(feedback)} />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </>
   );
