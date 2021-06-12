6eb941e33f0ae8ad7ef524dc8a5b3e1ece59b227
James Kelley <jkkelley86@gmail.com>
2021-06-12 07:18:18 -0500

    dcode cleanup

--- server/routes/feedBack.router.js
+++ server/routes/feedBack.router.js
@@ -6,15 +6,14 @@ const pool = require("../modules/pool");
 router.post("/", (req, res) => {
   // De-structure keys from the body
   const { feeling, understanding, support, comments } = req.body;
-  const flagged = true
   // Query to insert data
   const queryText = `
     INSERT INTO "feedback" (feeling, understanding, support, comments, flagged)
     VALUES
         ($1, $2, $3, $4, $5);
   `;
-
-  const values = [feeling, understanding, support, comments, flagged];
+  // Package our results up and get them ready for the Database.
+  const values = [feeling, understanding, support, comments];
 
   // Let's go for a swim to the database
   pool
